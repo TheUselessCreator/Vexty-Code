5ac3bbc3339008ca574caf03c153bff6411cbc01
TheUselessCreator <bot40games2@gmail.com>
2024-09-22 17:09:06 -0400

    Update tokens.js

--- assets/tokens.js
+++ assets/tokens.js
@@ -25,7 +25,49 @@ const tokenDefinitions = [
     { regex: /^\|\|/, type: 'OR' },          // Logical OR
     { regex: /^!/, type: 'NOT' },            // Logical NOT
     { regex: /^PRINT\b/, type: 'PRINT' },    // PRINT keyword
-    { regex: /^true\b|^false\b/, type: 'BOOLEAN' }  // Boolean values
+    { regex: /^true\b|^false\b/, type: 'BOOLEAN' },  // Boolean values
+    
+    // New Tokens
+    { regex: /^IF\b/, type: 'IF' },          // IF keyword
+    { regex: /^ELSE\b/, type: 'ELSE' },      // ELSE keyword
+    { regex: /^WHILE\b/, type: 'WHILE' },    // WHILE keyword
+    { regex: /^FOR\b/, type: 'FOR' },        // FOR keyword
+    { regex: /^DO\b/, type: 'DO' },          // DO keyword
+    { regex: /^SWITCH\b/, type: 'SWITCH' },  // SWITCH keyword
+    { regex: /^CASE\b/, type: 'CASE' },      // CASE keyword
+    { regex: /^DEFAULT\b/, type: 'DEFAULT' },// DEFAULT keyword
+    { regex: /^BREAK\b/, type: 'BREAK' },    // BREAK keyword
+    { regex: /^CONTINUE\b/, type: 'CONTINUE' }, // CONTINUE keyword
+    { regex: /^CONST\b/, type: 'CONST' },    // CONST keyword
+    { regex: /^LET\b/, type: 'LET' },        // LET keyword
+    { regex: /^TRY\b/, type: 'TRY' },        // TRY keyword
+    { regex: /^CATCH\b/, type: 'CATCH' },    // CATCH keyword
+    { regex: /^FINALLY\b/, type: 'FINALLY' },// FINALLY keyword
+    { regex: /^THROW\b/, type: 'THROW' },    // THROW keyword
+    { regex: /^NEW\b/, type: 'NEW' },        // NEW keyword
+    { regex: /^INSTANCEOF\b/, type: 'INSTANCEOF' }, // INSTANCEOF keyword
+    { regex: /^TYPEOF\b/, type: 'TYPEOF' },  // TYPEOF keyword
+    { regex: /^THIS\b/, type: 'THIS' },      // THIS keyword
+    { regex: /^IN\b/, type: 'IN' },          // IN keyword (for iterating)
+    { regex: /^DELETE\b/, type: 'DELETE' },  // DELETE keyword
+    { regex: /^EXTENDS\b/, type: 'EXTENDS' },// EXTENDS keyword (inheritance)
+    { regex: /^SUPER\b/, type: 'SUPER' },    // SUPER keyword
+    { regex: /^AWAIT\b/, type: 'AWAIT' },    // AWAIT keyword
+    { regex: /^ASYNC\b/, type: 'ASYNC' },    // ASYNC keyword
+    { regex: /^IMPORT\b/, type: 'IMPORT' },  // IMPORT keyword
+    { regex: /^EXPORT\b/, type: 'EXPORT' },  // EXPORT keyword
+    { regex: /^\.\.\./, type: 'SPREAD' },    // Spread operator (...)
+    { regex: /^\./, type: 'DOT' },           // Dot operator (.)
+    { regex: /^\[/, type: 'LBRACKET' },      // Left Bracket
+    { regex: /^\]/, type: 'RBRACKET' },      // Right Bracket
+    { regex: /^<\//, type: 'CLOSE_TAG' },    // Closing tag for HTML/XML-like code
+    { regex: /^</, type: 'LT' },             // Less than
+    { regex: /^>/, type: 'GT' },             // Greater than
+    { regex: /^<=/, type: 'LTE' },           // Less than or equal
+    { regex: /^>=/, type: 'GTE' },           // Greater than or equal
+    { regex: /^\?/, type: 'TERNARY_Q' },     // Ternary question mark
+    { regex: /^:/, type: 'TERNARY_COLON' },  // Ternary colon
+    { regex: /^MOD\b/, type: 'MOD' }         // MOD keyword
 ];
 
 module.exports = tokenDefinitions;
